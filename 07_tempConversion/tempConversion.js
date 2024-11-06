cdc623f00e9c3d7213eb5e61be44ae6030655e5b
cataclysmnik <sagniksinghaishere@gmail.com>
2024-11-06 19:27:31 +0530

    Finished exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) * (5 / 9);
+  if (Math.floor(c) != c) {
+    return Number(c.toFixed(1));
+  }
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c * (9 / 5) + 32;
+  if (Math.floor(f) != f) {
+    return Number(f.toFixed(1));
+  }
+  return f;
 };
 
 // Do not edit below this line
