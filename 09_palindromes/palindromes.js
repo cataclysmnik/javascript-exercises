478ad5dc3d2e93c2a43b79886863f6648977477d
cataclysmnik <sagniksinghaishere@gmail.com>
2024-11-06 20:38:35 +0530

    Finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let sentence = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/^[0-9a-z]+$/i)) {
+            sentence += str[i].toLowerCase();
+        }
+    }
 
+    let reverse = "";
+    for (let i = sentence.length - 1; i >= 0; i--) {
+        reverse += sentence[i];
+    }
+
+    return reverse === sentence;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
