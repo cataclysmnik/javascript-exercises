6443c7ac00df59123b0b7cdad8a1754a54938219
cataclysmnik <sagniksinghaishere@gmail.com>
2024-11-06 20:24:27 +0530

    Finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(x, y) {
+	return Number(x + y);
+};
+
+const subtract = function(x, y) {
+  return Number(x - y);
+};
+
+const sum = function(arr) {
+  let result = 0;
+  for (i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  let result = 1;
+  for (i = 0; i < arr.length; i++) {
+    result *= arr[i];
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const power = function(x,y) {
+  return x ** y;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fac = 1
+	if (n == 0) {
+    return 1;
+  }
+  for (i = 1; i <= n; i++) {
+    fac *= i
+  }
+  return fac;
 };
 
 // Do not edit below this line
