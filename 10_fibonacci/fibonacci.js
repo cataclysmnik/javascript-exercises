de2cbf0b9bfa48dcb1d07b5c3c784487fc954046
cataclysmnik <sagniksinghaishere@gmail.com>
2024-11-06 20:45:22 +0530

    Finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let sequence = [1, 1];
+    if (n == 0) {
+        return 0;
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    for (i = 0; i < n - 1; i++) {
+        sequence.push(sequence[i] + sequence[i+1]);
+    }
+    return sequence[n-1]
 };
 
 // Do not edit below this line
