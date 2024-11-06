09569124181940415c31babbc627936df75909c7
cataclysmnik <sagniksinghaishere@gmail.com>
2024-11-06 19:04:55 +0530

    Finished exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let result = 0;
+    if (typeof x != "number" || typeof y != "number") {
+        return("ERROR")
+    }
+    if (x < 0 || y < 0) {
+        return("ERROR")
+    }
+    if (Math.floor(x) != x || Math.floor(y) != y) {
+        return("ERROR")
+    }
+    if (y > x) {
+        result = y;
+        for (i = x; i < y; i++) {
+            result += i;
+        }
+    }
+    else if (x > y) {
+        result = x;
+        for (i = y; i < x; i++) {
+            result += i;
+        }
+    };
 
+
+    return result;
 };
 
 // Do not edit below this line
